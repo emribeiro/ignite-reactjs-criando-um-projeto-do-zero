9e5b899e051003833b838595d3a22f661a174af6
Eric Miranda Ribeiro <ktoxlonian@gmail.com>
2022-01-17 12:45:51 -0300

    Post Return from Prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { GetStaticProps } from 'next';
 
+import { Head } from 'next/document';
+import Prismic from '@prismicio/client';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,13 +26,69 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export default function Home({ postsPagination }: HomeProps) {
+  return (
+    <>
+      <div>
+        <div>
+          <h3>spacetraveling</h3>
+        </div>
+        {postsPagination.results.map(post => (
+          <>
+            <h1>{post.data.title}</h1>
+            <p>{post.data.subtitle}</p>
+            <div>
+              <time>{post.first_publication_date}</time>
+              <span>{post.data.author}</span>
+            </div>
+          </>
+        ))}
+      </div>
+    </>
+  );
+}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    Prismic.predicates.at('document.type', 'blog_post'),
+    {
+      fetch: [
+        'blog_post.title',
+        'blog_post.subtitle',
+        'blog_post.author',
+        'blog_post.content',
+      ],
+      pageSize: 2,
+    }
+  );
 
-//   // TODO
-// };
+  const postPagination: PostPagination = {
+    next_page: postsResponse.next_page,
+    results: postsResponse.results.map(post => {
+      return {
+        uid: post.uid,
+        first_publication_date: new Date(
+          post.first_publication_date
+        ).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric',
+        }),
+        data: {
+          title: post.data.title,
+          subtitle: post.data.subtitle,
+          author: post.data.author,
+        },
+      };
+    }),
+  };
+
+  return {
+    props: {
+      postsPagination: postPagination,
+    },
+  };
+};
